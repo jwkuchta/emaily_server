2fc35ff4d9290a0981971e3679afe827c3b75115
Justina Kuchta <justyna.kuchta@gmail.com>
2020-08-06 17:06:59 -0400

    added google client id and secret

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20')
 
 // generates a new application, used to setup config to listen to incoming requests and directing them to the right handler
 const app = express()
 
 // create route handler and associate with the route
-app.get('/', (req, res) => {
-    res.send({lala: ';a;a;a'})
-})
+// we no longer need it for oauth
+// app.get('/', (req, res) => {
+//     res.send({lala: ';a;a;a'})
+// })
+
+// we tell passport to create an instance of this particular strategy
+passport.use(new GoogleStrategy())
 
 // heroku injects environment variables telling us what port is dynamically set
 const PORT = process.env.PORT || 5000
