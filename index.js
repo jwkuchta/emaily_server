b293a4b6d952a036a5b401845cfc8fb5ad2c1b7f
Justina Kuchta <justyna.kuchta@gmail.com>
2020-08-06 19:49:55 -0400

    configured index.js for google auth flow

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20')
+const keys = require('./keys')
 
 // generates a new application, used to setup config to listen to incoming requests and directing them to the right handler
 const app = express()
@@ -12,7 +13,25 @@ const app = express()
 // })
 
 // we tell passport to create an instance of this particular strategy
-passport.use(new GoogleStrategy())
+passport.use(
+    new GoogleStrategy({
+        // has to be ID not Id!!!
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: 'google/callback'
+    }, accessToken => {
+        console.log(accessToken)
+    })
+)
+
+// we need to add a route handler to make sure the user is redirected to auth
+// we will tell express to involve passport
+// passport know to use 'google' as an instance of GoogleStrategy, it is an internal identifier
+app.get('/auth/google', passport.authenticate(
+    'google', {
+        scope: ['profile', 'email']
+    }
+))
 
 // heroku injects environment variables telling us what port is dynamically set
 const PORT = process.env.PORT || 5000
