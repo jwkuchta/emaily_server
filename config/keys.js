319d51dc6624bde62017c1c06fb70b75986c82fa
Justina Kuchta <justyna.kuchta@gmail.com>
2020-08-08 10:23:01 -0400

    double-checked keys for redeploy

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,6 @@
 // Heroku sets process.env.NODE_ENV when we configure env variables on Heroku
 if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./prod')
+    module.exports = require('./prod');
 } else {
-    // this way we both export and require in the dev keys
-    module.exports = require('./dev')
+    module.exports = require('./dev');
 }
