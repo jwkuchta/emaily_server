32a12293702718b7a886a96caf5144ae8aa71621
Justina Kuchta <justyna.kuchta@gmail.com>
2020-08-11 10:40:58 -0400

    conditional rendering of the header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class Header extends Component {
+
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return 'Still deciding'
+            case false:
+                return 'You are not logged in'
+            default:
+                return 'You are logged int'
+        }
+    }
+    
     render() {
         return (
             <div>
@@ -8,7 +21,7 @@ class Header extends Component {
                     <div className="nav-wrapper">
                         <a href="#" className="left brand-logo">emaily</a>
                         <ul className="right">
-                            <li><a>Login with Google</a></li>
+                        {this.renderContent()}
                         </ul>
                     </div>
                 </nav>
@@ -17,4 +30,14 @@ class Header extends Component {
     }
 }
 
-export default Header
+// const mapSTP = state => {
+//     return {
+//         auth: state.auth
+//     }
+// }
+
+const mapSTP = ({ auth }) => {
+    return { auth }
+}
+
+export default connect(mapSTP)(Header)
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,7 +1,11 @@
-export default function(state = {}, action) {
-    console.log(action)
+import { FETCH_USER } from "../actions/types"
+import { bindActionCreators } from "redux"
+
+export default function(state = null, action) {
     console.log(action.payload)
     switch (action.type) {
+        case FETCH_USER:
+            return action.payload || false
         default:
             return state
     }
