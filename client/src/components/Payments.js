251d62022840ecf8d82b8ef5e21c31f4c62fa989
Justina Kuchta <justyna.kuchta@gmail.com>
2020-08-12 11:44:52 -0400

    made button prettier

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -5,10 +5,14 @@ class Payments extends Component {
     render() {
         return (
             <StripeCheckout 
+            name='Emaily'
+            description='$5 for 5 credits'
             amount={500} // 500 cents
             token={token => console.log(token)} // callback after we get authorization object from Stripe api
             stripeKey={process.env.REACT_APP_STRIPE_KEY}
-            />
+            >
+                <button className='btn'>Add Credits</button>
+            </StripeCheckout>
         )
     }
 }
