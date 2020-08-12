5251d3f912b4fc94f085b63913a630830d4abd6a
Justina Kuchta <justyna.kuchta@gmail.com>
2020-08-12 10:51:56 -0400

    stripe keys configured locally and in heroku

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,13 +8,23 @@ module.exports = app => {
     ))
     
     // at this point we have the code and passport will handle the token/code
-    app.get('/auth/google/callback', passport.authenticate('google'))
+    app.get(
+        '/auth/google/callback', 
+        passport.authenticate('google'),
+        (req, res) => {
+            res.redirect('/surveys') // we want to redirect the user after they've been authenticated
+        }
+    )
 
     // destroys the cookie
-    app.get('/api/logout', (req, res) => {
-        req.logout()
-        res.send(req.user)
-    })
+    app.get(
+        '/api/logout', 
+        (req, res) => {
+            req.logout()
+            res.redirect('/')
+            // res.send(req.user)
+        }
+    )
 
     // if we open this tab after logging in, it will have the cookie 
     app.get('/api/current_user', (req, res) => res.send(req.user))
