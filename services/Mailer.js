38e8bbd0b523e6b4631b755dbf3e6897e575986f
Justina Kuchta <justyna.kuchta@gmail.com>
2020-08-17 19:48:26 -0400

    more mailer config

--- services/Mailer.js
+++ services/Mailer.js
@@ -3,5 +3,13 @@ const helper = sendgrid // convention - it helps us create the mailer
 const keys = require('../config/keys')
 
 class Mailer extends helper.Mail {
-    
+    constructor({ subject, recipient }, content) {
+        super()
+        this.from_email = new helper.Email('justyna.kuchta@gmail.com')
+        this.subject = subject
+        this.body = new helper.Content('text/html', content)
+        this.recipients = this.formatAddresses(recipients)
+    }
 }
+
+module.exports = Mailer
