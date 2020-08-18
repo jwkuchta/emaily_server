ca1d53b63a680701eac0466c572b15c5d70ea47c
Justina Kuchta <justyna.kuchta@gmail.com>
2020-08-18 15:12:50 -0400

    switch back the code

--- services/Mailer.js
+++ services/Mailer.js
@@ -1,23 +1,79 @@
-const sgMail = require("@sendgrid/mail")
-const keys = require("../config/keys")
+// const sgMail = require("@sendgrid/mail")
+// const keys = require("../config/keys")
  
-class Mailer {
+// class Mailer {
+//   constructor({ subject, recipients }, content) {
+//     // sgMail.setApiKey(keys.sendGridKey) // 
+//     sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+//     this.msg = {
+//       to: recipients.map(({ email }) => email),
+//       from: "no-reply@emaily.com",
+//       subject: subject,
+//       html: content,
+//       trackingSettings: { enable_text: true, enabled: true }
+//     };
+//   }
+ 
+//   async send() {
+//     const response = await sgMail.send(this.msg)
+//     return response;
+//   }
+// }
+ 
+// module.exports = Mailer
+
+const sendgrid = require('sendgrid');
+const helper = sendgrid.mail;
+const keys = require('../config/keys');
+
+class Mailer extends helper.Mail {
   constructor({ subject, recipients }, content) {
-    // sgMail.setApiKey(keys.sendGridKey) // 
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-    this.msg = {
-      to: recipients.map(({ email }) => email),
-      from: "no-reply@emaily.com",
-      subject: subject,
-      html: content,
-      trackingSettings: { enable_text: true, enabled: true }
-    };
+    super();
+
+    this.sgApi = sendgrid(keys.sendGridKey);
+    this.from_email = new helper.Email('justyna.kuchta@gmail.com');
+    this.subject = subject;
+    this.body = new helper.Content('text/html', content);
+    this.recipients = this.formatAddresses(recipients);
+
+    this.addContent(this.body);
+    this.addClickTracking();
+    this.addRecipients();
   }
- 
+
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => {
+      return new helper.Email(email);
+    });
+  }
+
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
+
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
+  }
+
+  addRecipients() {
+    const personalize = new helper.Personalization();
+
+    this.recipients.forEach(recipient => {
+      personalize.addTo(recipient);
+    });
+    this.addPersonalization(personalize);
+  }
+
   async send() {
-    const response = await sgMail.send(this.msg)
+    const request = this.sgApi.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: this.toJSON()
+    });
+
+    const response = this.sgApi.API(request);
     return response;
   }
 }
- 
-module.exports = Mailer
+
+module.exports = Mailer;
