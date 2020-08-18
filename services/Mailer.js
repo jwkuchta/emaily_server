a6adfba26179991f2f13db22816fc39201f68f90
Justina Kuchta <justyna.kuchta@gmail.com>
2020-08-18 15:31:36 -0400

    updated Mailer.js typos on line 2 and 39

--- services/Mailer.js
+++ services/Mailer.js
@@ -1,27 +1,3 @@
-// const sgMail = require("@sendgrid/mail")
-// const keys = require("../config/keys")
- 
-// class Mailer {
-//   constructor({ subject, recipients }, content) {
-//     // sgMail.setApiKey(keys.sendGridKey) // 
-//     sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-//     this.msg = {
-//       to: recipients.map(({ email }) => email),
-//       from: "no-reply@emaily.com",
-//       subject: subject,
-//       html: content,
-//       trackingSettings: { enable_text: true, enabled: true }
-//     };
-//   }
- 
-//   async send() {
-//     const response = await sgMail.send(this.msg)
-//     return response;
-//   }
-// }
- 
-// module.exports = Mailer
-
 const sendgrid = require('sendgrid');
 const helper = sendgrid.mail;
 const keys = require('../config/keys');
