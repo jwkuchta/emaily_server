65d4038b692691027cbda51de687f2909de00f45
Justina Kuchta <justyna.kuchta@gmail.com>
2020-08-18 09:35:20 -0400

    finished Mailer configuration now really

--- services/Mailer.js
+++ services/Mailer.js
@@ -3,15 +3,15 @@ const helper = sendgrid // convention - it helps us create the mailer
 const keys = require('../config/keys')
 
 class Mailer extends helper.Mail {
-    constructor({ subject, recipient }, content) {
+    constructor({ subject, recipients }, content) {
         super()
+        this.sendgridApi = sendgrid(keys.sendgridApiKey) // to communicate with the sendgrid Api
         this.from_email = new helper.Email('justyna.kuchta@gmail.com')
         this.subject = subject
         this.body = new helper.Content('text/html', content)
         this.recipients = this.formatAddresses(recipients)
 
         this.addcontent(this.body)
-
         // enable click tracking inside our email
         this.addClickTracking()
         this.addRecipients()
@@ -37,6 +37,18 @@ class Mailer extends helper.Mail {
         const personalize = new helper.Personalization()
         this.recipients.forEach(recipient =>  personalize.addTo(recipient))
         this.addPersonalization(personalize)
+    }
+
+    // actually send it to SendGrid
+    async send() {
+        const request = this.sendgridApi.emptyRequest({
+            method: 'POST',
+            path: '/v3/mail/send',
+            body: this.toJSON()
+        })
+
+        const response = this.sendgridApi.API(request)
+        return response
     }
 }
 
