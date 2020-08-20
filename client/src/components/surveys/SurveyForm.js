8ef7b75dc972fa2abcbc3ad3f64eb209d1f8d221
Justina Kuchta <justyna.kuchta@gmail.com>
2020-08-20 11:33:04 -0400

    made buttons pretty

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -2,6 +2,7 @@ import _ from 'lodash'
 import React, { Component } from 'react'
 // reduxForm allows us to communicate with reduxStore (nearly identical to connect)
 // Field component needs props to work (3 required pros are: type, name, and component)
+import { Link } from 'react-router-dom'
 import { reduxForm, Field } from 'redux-form' 
 import SurveyField from './SurveyField'
 
@@ -38,7 +39,11 @@ class SurveyForm extends Component {
                 {/* handleSubmit comes from reduxForm */}
                 <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
                 {this.renderFields()}
-                <button type="submit">Submit</button>
+                <Link to="/surveys" className="red btn-flat left white-text">Cancel</Link>
+                <button type="submit" className="teal btn-flat right white-text">
+                    Next
+                    <i className="material-icons right">done</i>
+                </button>
                 </form>
             </div>
         )
